c60719a0c16e7c7cf0fd814c7ccd80c09ed0a04a
adquemOsvaldo <osalazar@adquem.com>
2013-11-04 18:40:23 -0600

    Linea modificada
    Linea modificada en el github

--- src/com/example/pruebagithub/MainActivity.java
+++ src/com/example/pruebagithub/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		System.out.println("esta es una modificacion del proyecto nuevo");
 		return true;
 	}
 
